6315a41b62195c52089947172c147f3a2e69dc94
Ryan O'Keeffe <okeefferd@gmail.com>
2015-03-09 16:27:32 -0500

    support inputs at the base of the bucket, exclude output and archive prefixes

--- s3-csv2json.py
+++ s3-csv2json.py
@@ -2,7 +2,8 @@ __author__ = "Ryan O'Keeffe"
 
 ## This script will take AWS billing information file in an s3 bucket with specified prefix in the form of csv with a
 ## header and convert it to line based json for ingestion by things like Logstash.  Files will be backed up to the same
-##  bucket under the 'backup_prefix' and converted files will be placed in the 'destination_prefix'
+## bucket under the 'backup_prefix' and converted files will be placed in the 'destination_prefix'
+## Currently also handles zip files containing a single file, named the same as the archive sans the .zip extension
 
 import boto
 import csv
@@ -10,22 +11,34 @@ import json
 import getopt
 import sys
 import re
+import os
+import zipfile
+
+source_bucket = ''
+region_endpoint = 'us-east-1'
+input_prefix = ''
+output_prefix = ''
+backup_prefix = ''
+temp_dir = '/tmp/'
 
 def main(argv):
-  source_bucket = ''
-  region_endpoint = 'us-east-1'
-  input_prefix = ''
-  output_prefix = ''
-  backup_prefix = ''
+
+  global source_bucket
+  global region_endpoint
+  global input_prefix
+  global output_prefix
+  global archive_prefix
+  global temp_dir
+
 
   try:
-    opts, args = getopt.getopt(argv, "s:i:o:b:r:")
+    opts, args = getopt.getopt(argv, "s:i:o:a:r:")
   except getopt.GetoptError:
-    print 's3-csv2json.py -s <source bucket> -i <input prefix> -o <output prefix> -b <backup prefix> -r <region | us-east-1 default>'
+    print 's3-csv2json.py -s <source bucket> -i <input prefix> -o <output prefix> -a <archive prefix> -r <region | us-east-1 default>'
     sys.exit(2)
   for opt, arg in opts:
     if opt == '-h':
-      print 's3-csv2json.py -s <source bucket> -i <input prefix> -o <output prefix> -b <backup prefix> -r <region | us-east-1 default>'
+      print 's3-csv2json.py -s <source bucket> -i <input prefix> -o <output prefix> -a <archive prefix> -r <region | us-east-1 default>'
       sys.exit()
     elif opt in ("-s"):
       source_bucket = arg
@@ -33,14 +46,18 @@ def main(argv):
       input_prefix = arg
     elif opt in ("-o"):
       output_prefix = arg
-    elif opt in ("-b"):
-      backup_prefix = arg
+    elif opt in ("-a"):
+      archive_prefix = arg
     elif opt in ("-r"):
       region_endpoint = arg
 
-  if source_bucket == '' or input_prefix == '' or output_prefix == '' or backup_prefix == '':
-    print 'Missing required argument, usage: s3-csv2json.py -s <source bucket> -i <input prefix> -d <destination prefix> -b <backup prefix> -r <region | us-east-1 default>'
+  if source_bucket == '' or output_prefix == '' or archive_prefix == '':
+    print 'Missing required argument, usage: s3-csv2json.py -s <source bucket> -i <input prefix> -d <destination prefix> -b <archive_prefix> -r <region | us-east-1 default>'
     sys.exit(2)
+
+  # Ensure we have our temp dir
+  if not os.path.exists(temp_dir):
+    os.makedirs(temp_dir)
 
   # Create the connection
   s3conn = boto.s3.connect_to_region(region_endpoint)
@@ -50,23 +67,57 @@ def main(argv):
 
   # Do the work
   for existing_key in s3bucket.list(prefix=input_prefix):
-    if not(existing_key.name.endswith('/')):
+    # Ignore folders, the output prefix, and the archive_prefix
+    if not(existing_key.name.endswith('/') or existing_key.name.startswith(output_prefix) or existing_key.name.startswith(archive_prefix)):
       print "Found key: " + existing_key.name
       regex = re.compile('^%s(.*)'%input_prefix)
       key_no_prefix = regex.match(existing_key.name).group(1)
+      download_key(existing_key, temp_dir + key_no_prefix)
+      processed_filename = process_file(temp_dir + key_no_prefix)
       newkey = s3bucket.new_key(output_prefix + key_no_prefix + ".json")
-      print "Processing contents of: " + existing_key.name
-      key_contents= existing_key.get_contents_as_string()
-      key_csv = csv.DictReader(key_contents.splitlines(), delimiter=',', quotechar='"')
-      key_json = ''
-      for row in key_csv:
-        key_json+=json.dumps(row)
-        key_json+='\n'
-      print "Backing up key:" + existing_key.name
-      existing_key.copy(dst_bucket=source_bucket, dst_key=backup_prefix + key_no_prefix)
-      existing_key.delete()
-      print "Outputting json to: " + newkey.name
-      newkey.set_contents_from_string(key_json)
+      upload_result(newkey, processed_filename)
+      # Don't 'archive' unless we succeed in processing
+      backup_key(existing_key, backup_prefix + key_no_prefix)
+      os.remove(processed_filename)
+
+def process_file(filename):
+  print "Getting filehandles"
+  if zipfile.is_zipfile(filename):
+    ziphandle = open(filename, 'rb')
+    zip = zipfile.ZipFile(ziphandle)
+    # Usecase is our zip is one file named the same as our zip minus extension
+    filename_clean = re.match('^(.*)\.zip$', os.path.basename(filename)).group(1)
+    print filename_clean
+    zipfh = zip.open(filename_clean)
+    rv = process_csv(filename, zipfh)
+    os.remove(filename)
+    return rv
+  else:
+    rv = process_csv(filename, open(filename, 'r'))
+    os.remove(filename)
+    return rv
+
+def process_csv(filename, filehandle):
+  print "Processing contents of: " + filename
+  jsonfile = open(filename + ".json", 'w')
+  reader = csv.DictReader(filehandle)
+  for row in reader:
+    json.dump(row, jsonfile)
+    jsonfile.write('\n')
+  return filename + ".json"
+
+def upload_result(dest_key, local_filename):
+  print "Outputting json to: " + dest_key.name
+  dest_key.set_contents_from_filename(local_filename)
+
+
+def backup_key(remote_key, dest_key):
+  print "Backing up key:" + remote_key.name
+  remote_key.copy(dst_bucket=source_bucket, dst_key=dest_key)
+  remote_key.delete()
+
+def download_key(remote_key, local_filename):
+  remote_key.get_contents_to_filename(local_filename)
 
 if __name__ == "__main__":
   main(sys.argv[1:])
